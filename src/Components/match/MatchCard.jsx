5941fe02df75b03da4328a2092ff4acff8de544c
unknown <i.sookyung93@gmail.com>
2026-02-04 16:07:25 +0900

    add prev/next match card & css

---
+++ src/Components/match/MatchCard.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./MatchCard.module.css";
+import { logoMap } from "../../constants/logoMap";
+
+export default function MatchCard({ type, game }) {
+  console.log(game);
+
+  const isFinished = game.status === "종료";
+  const teams = game.home
+    ? {
+        left: {
+          name: game.opponent,
+          logo: logoMap[game.opponent],
+          ...(isFinished && { score: game.score.opponentScore }),
+        },
+        right: {
+          name: "두산",
+          logo: "doosan",
+          ...(isFinished && { score: game.score.doosanScore }),
+        },
+      }
+    : {
+        left: {
+          name: "두산",
+          logo: "doosan",
+          ...(isFinished && { score: game.score.doosanScore }),
+        },
+        right: {
+          name: game.opponent,
+          logo: logoMap[game.opponent],
+          ...(isFinished && { score: game.score.opponentScore }),
+        },
+      };
+  return (
+    <div className={styles.card}>
+      <p className={styles.date}>{game.date}</p>
+      <div className={styles.content}>
+        <div className={styles.team}>
+          <div className={styles.imgBox}>
+            <img src={`/images/logo/${teams.left.logo}.png`} alt="" />
+          </div>
+          <p>{teams.left.name}</p>
+        </div>
+        {isFinished ? (
+          <section className={styles.gameScore}>
+            <p>{teams.left.score}</p>
+            <p>:</p>
+            <p>{teams.right.score}</p>
+          </section>
+        ) : (
+          <p className={styles.gameStatus}>{game.status}</p>
+        )}
+        <div className={styles.team}>
+          <div className={styles.imgBox}>
+            <img src={`/images/logo/${teams.right.logo}.png`} alt="" />
+          </div>
+          <p>{teams.right.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import styles from "./Home.module.css";
+import MatchCard from "../../Components/match/MatchCard";
+import useGamesSchedule from "../../hooks/usegamesSchedule";
+
+const todayDate = new Date();
+const FINISHED_STATUS = ["종료", "취소"];
 
 export default function Home() {
+  const { games, loading } = useGamesSchedule();
+
+  if (loading) {
+    return <div>로딩중...</div>;
+  }
+
+  let prevGame = null;
+  let nextGame = null;
+
+  for (const game of games) {
+    if (FINISHED_STATUS.includes(game.status)) {
+      prevGame = game;
+    } else {
+      nextGame = game;
+      break;
+    }
+  }
+
   return (
     <section className={styles.home}>
       <article className={styles.bubble}>
         <span className={styles.bubbleText}>최강10번타자</span>
       </article>
       <article className={styles.centerArea}>
-        <div className={styles.gameCard}>이전경기</div>
+        <MatchCard type="prevMatch" game={prevGame} />
         <div className={styles.gomBox}>
           <img src="images/gom_home.png" alt="곰_홈" />
         </div>
-        <div className={styles.gameCard}>다음경기</div>
+        <MatchCard type="nextMatch" game={nextGame} />
       </article>
     </section>
   );
