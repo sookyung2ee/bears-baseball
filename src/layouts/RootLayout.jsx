852e9366f1b715f6f39ded6e65d4f2ba602ac27c
unknown <i.sookyung93@gmail.com>
2026-03-25 15:33:38 +0900

    style: fix scroll top, fix sticky header, update calendar button size

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router-dom";
 import styles from "./RootLayout.module.css";
 import Navbar from "../Components/layout/Navbar";
 import TopNavbar from "../Components/layout/TopNavbar";
+import ScrollToTop from "../Components/ScrollToTop";
 
 export default function RootLayout() {
   return (
     // <></>
     <div className={styles.root}>
+      <ScrollToTop />
       <div className={styles.mobileHeader}>
         <TopNavbar />
       </div>
